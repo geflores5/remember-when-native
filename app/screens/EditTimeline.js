3cd933589a019b3cba13e3a67075bc0d8cc74081
Giovanni Flores <geflores@Pip-Boy.local>
2019-10-24 01:23:33 -0700

    fix removetimeline removing memories

--- app/screens/EditTimeline.js
+++ app/screens/EditTimeline.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Button, Text, TextInput, View } from 'react-native';
 
 import { editTimeline, removeTimeline } from '../actions/timelines';
+import { removeMemory } from '../actions/memories';
 import TimelineForm from '../components/TimelineForm/TimelineForm';
 
 class EditTimelinePage extends Component {
@@ -16,20 +17,23 @@ class EditTimelinePage extends Component {
 
   render() {
     console.log(this.props.navigation.state.params.timeline.id)
-    const currentTimeline = this.props.navigation.state.params.timeline;
+    const currentTimeline = this.props.timeline;
     return (
       <View>
         <TimelineForm
           timeline={currentTimeline}
           submitTimeline={timeline => {
-            this.props.dispatch(editTimeline(currentTimeline.id, timeline));
+            this.props.editTimeline(currentTimeline.id, timeline);
           }}
           goHome={this.handleGoHome}
         />
         <Button
           title="Remove Timeline"
           onPress={() => {
-            this.props.dispatch(removeTimeline(currentTimeline.id));
+            this.props.removeTimeline(currentTimeline.id);
+            this.props.memories && this.props.memories.map(memory => (
+              this.props.removeMemory(memory.id)
+            ));
             this.handleGoHome();
           }}
         />
@@ -38,4 +42,20 @@ class EditTimelinePage extends Component {
   }
 }
 
-export default connect()(EditTimelinePage);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    editTimeline: (id, timeline) => dispatch(editTimeline(id, timeline)),
+    removeTimeline: (id) => dispatch(removeTimeline(id)),
+    removeMemory: (id) => dispatch(removeMemory(id))
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    timeline: state.firestore.ordered.timelines.find((timeline) => timeline.id === ownProps.navigation.state.params.timeline.id),
+    memories: state.firestore.ordered.memories && state.firestore.ordered.memories.filter((memory) => memory.timelineID === ownProps.navigation.state.params.timeline.id)
+  }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditTimelinePage);
